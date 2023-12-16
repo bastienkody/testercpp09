04057460a67be1e20270f1c50508c4a9a74fecef
BastienKody <basguilla@gmail.com>
2023-12-16 18:59:18 +0100

    tester directly beside makefile for input.txt name format

--- tester_cpp09ex00btc.sh
+++ tester_cpp09ex00btc.sh
@@ -4,9 +4,15 @@
 # 					------- EX 00 BTC EXCHANGE-------
 # ------------------------------------------------------------------------------
 
-#	expected files : Makefile, main.cpp, BitcoinExchange.{cpp, hpp} in repo ex00
-dir=./ex00/
-exec="./ex00/btc"
+#	clone tester repo inside your cpp09 main repo (cd cpp09 && gcl git@github.com:bastienkody/testercpp09.git)
+#	just copy the tester beside your makefile (cp testercpp09/tester_cpp09ex00btc.sh ./)
+#	launch : ./tester_cpp09ex00btc.sh 
+#	look on inputs.txt : cat testercpp09/inputstxt/*
+#	expected files in your cpp09/ex00 : Makefile, main.cpp, BitcoinExchange.{cpp, hpp}
+
+dir=./
+exec="./btc"
+input_dir="testercpp09/inputstxt/"
 
 #turn bool_ to 0 to skip specific tests
 bool_makefile=1
@@ -42,8 +48,8 @@ echo -e "${YEL_BG}Makefile${END}"
 #---------------
 # makefile relink
 echo -ne "${BLU_BG}Test Makefile${END}\t\t"
-make re -C $dir 1>/dev/null 2> stderrmake.txt
-make -C $dir > stdoutmakebis.txt 2>&1
+make re 1>/dev/null 2> stderrmake.txt
+make  > stdoutmakebis.txt 2>&1
 [[ -s stderrmake.txt ]] && echo -ne "${RED}make wrote on std err${END}" || echo -ne "${GREEN}no make error${END}" 
 echo -n " -- "
 cat stdoutmakebis.txt | egrep -viq "(nothin|already|date)" && echo -e "${RED}makefile relink?${END}" || echo -e "${GREEN}no relink${END}"
@@ -52,13 +58,13 @@ rm -rf stderrmake.txt stdoutmakebis.txt
 #-------------------------
 # makefile : flags and rule
 echo -ne "${BLU_BG}Makefile flags${END}\t\t"
-grep -sq -- "-Wall" ${dir}Makefile && echo -ne "${GREEN}OK (-Wall)${END}" || echo -ne "${RED}-Wall not found${END}"
+grep -sq -- "-Wall" Makefile && echo -ne "${GREEN}OK (-Wall)${END}" || echo -ne "${RED}-Wall not found${END}"
 echo -n " -- "
-grep -sq -- "-Wextra" ${dir}Makefile && echo -ne "${GREEN}OK (-Wextra)${END}" || echo -ne "${RED}-Wextra not found${END}"
+grep -sq -- "-Wextra" Makefile && echo -ne "${GREEN}OK (-Wextra)${END}" || echo -ne "${RED}-Wextra not found${END}"
 echo -n " -- "
-grep -sq -- "-Werror" ${dir}Makefile && echo -ne "${GREEN}OK (-Werror)${END}" || echo -ne "${RED}-Werror not found${END}"
+grep -sq -- "-Werror" Makefile && echo -ne "${GREEN}OK (-Werror)${END}" || echo -ne "${RED}-Werror not found${END}"
 echo -n " -- "
-grep -sq -- "-std=c++98" ${dir}Makefile && echo -ne "${GREEN}OK (std c++98)${END}" || echo -ne "${RED}Flag -std=c++98 not found${END}"
+grep -sq -- "-std=c++98" Makefile && echo -ne "${GREEN}OK (std c++98)${END}" || echo -ne "${RED}Flag -std=c++98 not found${END}"
 echo ""
 
 
@@ -87,43 +93,43 @@ echo "----------------------------------------------------------------"
 echo -e "${YEL_BG}Format error for input.txt${END}"
 
 echo -e "${BLUE_BG}1 - Empty line (linefeed only) : ${END}"
-cp inputstxt/input0.txt ./ && mv input0.txt input.txt
+cp ${input_dir}input0.txt && mv input0.txt input.txt
 ${exec} input.txt
 rm input.txt
 echo "----------------------------------------------------------------"
 
 echo -e "${BLUE_BG}1 - Multiple title line (date | value) : ${END}"
-cp inputstxt/input1.txt ./ && mv input1.txt input.txt
+cp ${input_dir}input1.txt ./ && mv input1.txt input.txt
 ${exec} input.txt
 rm input.txt
 echo "----------------------------------------------------------------"
 
 echo -e "${BLUE_BG}1 - No title line (date | value) : ${END}"
-cp inputstxt/input2.txt ./ && mv input2.txt input.txt
+cp ${input_dir}input2.txt ./ && mv input2.txt input.txt
 ${exec} input.txt
 rm input.txt
 echo "----------------------------------------------------------------"
 
 echo -e "${BLUE_BG}1 - Empty title line (date | value) : ${END}"
-cp inputstxt/input2bis.txt ./ && mv input2bis.txt input.txt
+cp ${input_dir}input2bis.txt ./ && mv input2bis.txt input.txt
 ${exec} input.txt
 rm input.txt
 echo "----------------------------------------------------------------"
 
 echo -e "${BLUE_BG}1 - Different name for title line (La date | valeur) : ${END}"
-cp inputstxt/input3.txt ./ && mv input3.txt input.txt
+cp ${input_dir}input3.txt ./ && mv input3.txt input.txt
 ${exec} input.txt
 rm input.txt
 echo "----------------------------------------------------------------"
 
 echo -e "${BLUE_BG}1 - Date erros (bad day/month/year, hexa, octal, overflow) : ${END}"
-cp inputstxt/input4.txt ./ && mv input4.txt input.txt
+cp ${input_dir}input4.txt ./ && mv input4.txt input.txt
 ${exec} input.txt
 rm input.txt
 echo "----------------------------------------------------------------"
 
 echo -e "${BLUE_BG}1 - Btc value erros : ${END}"
-cp inputstxt/input5.txt ./ && mv input5.txt input.txt
+cp ${input_dir}input5.txt ./ && mv input5.txt input.txt
 ${exec} input.txt
 rm input.txt
 echo "----------------------------------------------------------------"
